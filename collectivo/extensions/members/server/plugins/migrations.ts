d639445729c8403568bb23eaf8deffb4e0d86da8
Joël Foramitti <57440945+jofmi@users.noreply.github.com>
2023-10-03 11:03:49 +0200

    Don't use client in plugin

--- collectivo/extensions/members/server/plugins/migrations.ts
+++ collectivo/extensions/members/server/plugins/migrations.ts
@@ -4,7 +4,7 @@ export default defineNitroPlugin((nitroApp) => {
   if (!useRuntimeConfig().runMigrations) {
     return;
   }
-  const directus = useDirectus();
+  // const directus = useDirectus();
   // TODO: This calls 001 from core, not members
   // runMigrations();
 });
